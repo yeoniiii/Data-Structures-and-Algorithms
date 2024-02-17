36f6b048f175ceb0561c3e8bab82be1ad8e7657a
yeoniiii <sally879@naver.com>
2024-02-17 17:36:30 +0900

    3. 탐색 DFS/BFS

---
+++ 이것이코딩테스트다/5_DFS+BFS.py
@@ -0,0 +1,205 @@
+# 탐색
+## : 많은 양의 데이터 중에서 원하는 데이터를 찾는 과정
+
+
+# [이코테] 자료 구조 --------------------------------
+## 스택 : 선입후출
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.pop()
+
+print(stack)
+print(stack[::-1])
+
+
+## 큐 : 선입선출
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
+
+
+# [이코테] 그래프 --------------------------------
+## 표현 방식 1: 인접 행렬(Adgacency Matrix)
+INF = 999999999 # 무한의 비용 선언
+
+# 2차원 리스트를 이용해 인접 행렬 표현
+graph = [
+    [0, 7, 5],
+    [7, 0, INF],
+    [5, INF, 0]
+]
+
+print(graph)
+
+## 표현 방식 2: 인접 리스트(Adjacency List)
+# 행(Row)이 3개인 2차원 리스트로 인접 리스트 표현
+graph = [[] for _ in range(3)]
+
+# 노드 0에 연결된 노드 정보 저장 (노드, 거리)
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+
+# 노드 1에 연결된 노드 정보 저장 (노드, 거리)
+graph[1].append((0, 7))
+
+# 노드 2에 연결된 노드 정보 저장 (노드, 거리)
+graph[2].append((0, 5))
+
+print(graph)
+
+
+# [이코테] DFS: 깊이 우선 탐색 -------------------------------- 
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end = ' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]     
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited) # 1 2 7 6 8 3 4 5
+
+
+# [이코테] BFS: 너비 우선 탐색 --------------------------------
+from collections import deque
+
+# BFS 함수 정의
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 BFS 함수 호출
+bfs(graph, 1, visited)
+
+
+# [실전문제] 음료수 얼려 먹기 --------------------------------
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x, y):
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
+
+
+# [실전문제] 미로 탈출 --------------------------------
+from collections import deque
+
+# N, M을 공백을 기준으로 구분하여 입력 받기
+n, m = map(int, input().split())
+# 2차원 리스트의 맵 정보 입력 받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# BFS 소스코드 구현
+def bfs(x, y):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x, y))
+    # 큐가 빌 때까지 반복하기
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 4가지 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if graph[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    return graph[n - 1][m - 1]
+
+# BFS를 수행한 결과 출력
+print(bfs(0, 0))
